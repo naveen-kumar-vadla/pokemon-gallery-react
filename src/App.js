4112d6642e9659f9d837deabd7e971212be83345
Naveen Kumar Vadla <58026402+naveen-kumar-vadla@users.noreply.github.com>
2020-10-09 17:13:46 +0530

    modified app passing data from App to PokemonGallery

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 
 import PokemonGallery from './PokemonGallery.js';
+import * as data from './pokemonsData.json'
 
 const App = () => (
   <div>
     <h1 className='main-heading'>Pokemon Gallery</h1>
     <div className='mainContainer'>
-      <div className='searchBarContainer'></div>
-      <div>
-        <PokemonGallery />
-      </div>
+      <PokemonGallery pokemonsData={data.default}/>
     </div>
   </div>
 );
--- src/PokemonGallery.js
+++ src/PokemonGallery.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
 import PokemonCard from './PokemonCard.js'
-import * as data from './pokemonsData.json'
 
-const PokemonGallery = (_) => {
-  const pokemondsData = data.default;
-  const pokemons = pokemondsData.map(pokemon => <PokemonCard pokemon={pokemon} key={pokemon.name} />);
+const PokemonGallery = ({ pokemonsData}) => {
+  const pokemons = pokemonsData.map(pokemon => <PokemonCard pokemon={pokemon} key={pokemon.name} />);
   return <div className="pokemonContainer">{pokemons}</div>
 };
 
